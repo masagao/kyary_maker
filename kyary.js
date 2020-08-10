dcf1704209b5dea7b576a384e4c1b1c0eafe62cb
masagao <masanao@gaogao.asia>
2020-08-11 00:37:14 +0900

    add for loop

--- kyary.js
+++ kyary.js
@@ -6,7 +6,18 @@ function makeKyary() {
     document.getElementById('msg').innerHTML = msg;
   } else {
     var devName = orgName.split('');
-    var kyaryName = devName[0] + 'ゃ' + devName[1] + '-' + devName[2] + devName[3] + 'ゅ' + devName[2] + devName[3] + 'ゅ';
+    var kyaryName = '';
+    for (var i = 0; i < 4; i++) {
+      if (i == 0) {
+        kyaryName += devName[i] + 'ゃ';
+      } else if (i == 1) {
+        kyaryName += devName[i] + '-';
+      } else if (i == 2) {
+        kyaryName += devName[i] + devName[i + 1] + 'ゅ';
+      } else {
+        kyaryName += devName[i - 1] + devName[i] + 'ゅ';
+      }
+    }
     var msg = '・あなたの新しいお名前は 「' + kyaryName + '」';
     document.getElementById('msg').innerHTML = msg;
   }
